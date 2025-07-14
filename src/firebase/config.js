c796a13178f55350dbc294d4707c9e269d0ee657
Akassnidi Karunanithi <akashnidiofficial@gmail.com>
2025-07-14 20:36:30 +0530

    config changed

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,18 +1,24 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyClrySCTPrIQbbgkngypH4mKWzwmdel1RU",
+  apiKey: "AIzaSyClrySCTPrIQbbgkngypH4mKWzwmdel1dQ",
   authDomain: "moi-note-4c429.firebaseapp.com",
   projectId: "moi-note-4c429",
   storageBucket: "moi-note-4c429.firebasestorage.app",
   messagingSenderId: "847449157538",
-  appId: "1:847449157538:web:d95096b807ab4a86244c4e"
+  appId: "1:847449157538:web:d95096b807ab4a86244cfb"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+
+// Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
-export const storage = getStorage(app);
+
+export default app;
