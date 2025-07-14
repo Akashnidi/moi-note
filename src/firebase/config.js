5951e760b85690fa3abc101ffeb5742d7a83cce2
Akassnidi Karunanithi <akashnidiofficial@gmail.com>
2025-07-14 19:26:58 +0530

    syntax error fixed

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,15 +1,15 @@
-import { initializeApp } from 'firebase/app';
+mport { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore@
+import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClrySCTPrIQbbgkngypH4mKWzwmdel1dQ",
+  apiKey: "AIzaSyClrySCTPrIQbbgkngypH4mKWzwmdel1RU",
   authDomain: "moi-note-4c429.firebaseapp.com",
   projectId: "moi-note-4c429",
   storageBucket: "moi-note-4c429.firebasestorage.app",
   messagingSenderId: "847449157538",
-  appId: "1:847449157538:web:d95096b807ab4a86244cfb"
+  appId: "1:847449157538:web:d95096b807ab4a86244c4e"
 };
 
 const app = initializeApp(firebaseConfig);
