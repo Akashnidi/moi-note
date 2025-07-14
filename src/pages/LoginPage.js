257f6215435b355b777dbc14155d39e51a7c1f5d
Akassnidi Karunanithi <akashnidiofficial@gmail.com>
2025-07-14 19:36:06 +0530

    syntax error fixed

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-mport React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { signInWithEmailAndPassword, updatePassword } from 'firebase/auth';
 import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { auth, db } from '../firebase/config';
