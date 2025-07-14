3b07a9e47aa9978af177b9e15e2e36d8744f269f
Akassnidi Karunanithi <akashnidiofficial@gmail.com>
2025-07-14 19:16:28 +0530

    error fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-mport React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
